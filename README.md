023fcb7e57da5c8c98e13f338d1d2cc97a5c47fa
fansenze <151665030@qq.com>
2020-08-07 18:37:27 +0800

    🎉 init

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # snowpack-plugin-sass
-Use the node-sass to build .sass/.scss files from source
+Use the [node-sass](https://github.com/sass/node-sass) to build `.sass/.scss` files from source
+
+## Usage
+
+### Install
+```bash
+npm install --save-dev snowpack-plugin-sass
+```
+
+### Config
+add this plugin to your Snowpack config:  
+
+**snowpack.config.json**
+```json
+{
+  "plugins": [
+    "snowpack-plugin-sass"
+  ]
+}
+```
+
---
+++ plugin.js
@@ -0,0 +1,54 @@
+const compiler = require('node-sass');
+const fs = require('fs');
+const path = require('path');
+
+const pkg = require('./package.json');
+
+const render = async (filepath) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filepath, { encoding: 'utf-8' }, (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        const extname = path.extname(filepath);
+        compiler.render(
+          {
+            file: filepath,
+            data,
+            outputStyle: 'expanded',
+            // Set `indentedSyntax: false` to disable the indented syntax which is Sass’s original syntax,
+            // and enable the curly braces syntax for scss file.
+            indentedSyntax: extname === '.sass',
+          },
+          (err, result) => {
+            if (err) {
+              reject(err);
+            } else {
+              resolve(result);
+            }
+          }
+        );
+      }
+    });
+  });
+}
+
+module.exports = function plugin(snowpackConfig) {
+  return {
+    name: pkg.name,
+    resolve: {
+      input: ['.sass', '.scss'],
+      output: ['.css'],
+    },
+    async load({ filePath }) {
+      try {
+        const result = await render(filePath);
+        return {
+          '.css': result.css.toString(),
+        };
+      } catch (err) {
+        console.error(err);
+      }
+    },
+  };
+}
